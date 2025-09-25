fb937480bc6c7cd762b2d9574d4180dd1b0cac8e
tiqidini <dubolazoff@gmail.com>
2025-09-25 20:17:48 +0300

    chore: Trigger deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pribori-pwa-clone/',
+  base: '/tiqidini-pribori-pwa-clone/',
   plugins: [react()],
 })
